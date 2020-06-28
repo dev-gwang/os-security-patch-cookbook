a848e1f6cc3e16da950494d3b81a8e9a8ea8a1c8
gwangil <pki054@naver.com>
2020-06-28 15:20:35 +0900

    [d98] MySQL 패스워드 입력 시, 계정 정보 role에 저장하도록 수정

--- recipes/d98-weak-password-remove.rb
+++ recipes/d98-weak-password-remove.rb
@@ -4,7 +4,11 @@
 #
 # Copyright:: 2019, The Authors, All Rights Reserved.
 
+mysql_user_id = node["d98-weak-password-remove"]["mysql_admin_id"]["value"]
+mysql_user_password = node["d98-weak-password-remove"]["mysql_admin_password"]["value"]
+
 execute "remove non user" do
     user "root"
-    command "mysql -uroot -e \"DELETE FROM mysql.user WHERE User=''\""
+    command "mysql -u#{mysql_user_id} -p#{mysql_user_password} -e \"DELETE FROM mysql.user WHERE User=''\""
+    only_if "ps -ef | grep mysql | grep -v mysql"
 end
