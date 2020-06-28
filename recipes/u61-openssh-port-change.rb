5aacb4504c4839640b3fe849f8d474a82a91d937
gwangil <pki054@naver.com>
2020-06-28 15:27:09 +0900

    OpenSSH 경로 role 추가

--- recipes/u61-openssh-port-change.rb
+++ recipes/u61-openssh-port-change.rb
@@ -5,12 +5,13 @@
 # Copyright:: 2019, The Authors, All Rights Reserved.
 
 openssh_port = `netstat -tnlp | grep ssh | grep "0.0.0.0" | awk {'print $4'} | awk -F: {'print $2'}`.to_s
+openssh_path = node["u61-openssh-port-change"]["openssh-path"]["value"]
 
 bash "openssh sshd_config patch" do
     user "root"
     code <<-EOH
-        cat /etc/ssh/sshd_config | grep -v Port > /tmp/sshd_config_bak;
-        cp -rfp /tmp/sshd_config_bak /etc/ssh/etc/sshd_config;
+        cat #{openssh_path}/sshd_config | grep -v Port > /tmp/sshd_config_bak;
+        cp -rfp /tmp/sshd_config_bak #{openssh_path}/sshd_config;
     EOH
     only_if {"#{openssh_port}" == "22\n"}
 end
